1971c5a3fb9cf1fe0a47ec1e4ac6cdb5d39a4412
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-22 18:08:16 -0400

    commit tree f2c424ae2dab1c9ac9d4c2c1fff518b5eb2edcac

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(sat = "roller-skate") {
+    return `This Saturday, I want to ${sat}!`
+}
+saturdayFun();
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+
+function wrapAdjective() {
+    return function () {
+        return `You are `
+    }    
+}
